faf2ffea51050d06d518f7da6c477d9223c7caec
trandung2k1 <trandungitksnb2001@gmail.com>
2023-06-11 09:31:51 +0700

    #1 fix issues app running before connectdb

--- nestjs_mssql/src/main.ts
+++ nestjs_mssql/src/main.ts
@@ -1,14 +1,13 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { INestApplication } from '@nestjs/common';
 import { connectDB } from './config';
-
 async function bootstrap() {
-  const app: INestApplication = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  await connectDB();
+  setTimeout(async () => {
+    const app: INestApplication = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT);
+  }, 3000);
 }
 
-(async () => {
-  await connectDB();
-  await bootstrap();
-})();
+bootstrap();
